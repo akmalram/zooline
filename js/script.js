d02e16caa875bdd9e8a58d381cdf5c42fcb1e9bc
Akmal <islamov_akmal@inbox.ru>
2019-10-12 16:35:58 +0500

    some

---
+++ js/script.js
@@ -0,0 +1,20 @@
+window.addEventListener('load', () => {
+    let menuToggler = () => {
+        let btn = document.querySelector('header .btn-toggle');
+        let menu = document.querySelector('nav .menu');
+
+        btn.addEventListener('click', () => {
+            btn.classList.toggle('active');
+            menu.classList.toggle('active');
+        });
+        window.addEventListener('resize', (e) => {
+            if(window.innerWidth > 720) {
+                btn.classList.remove('active');
+                menu.classList.remove('active');
+            }
+        })
+    }
+
+    menuToggler();
+
+});
